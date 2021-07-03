3efee0a9ddb03cf4cb2a09d0cf882b3000a8b543
Stephanie Wagner <stephwag@stephwag.com>
2021-07-03 16:40:20 -0400

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# nsgadget-scripts
-Scripts/examples to automate inputs in Nintendo Switch games by plugging in a Teensy into a Switch
+# NSGadget Scripts
+Scripts/examples to automate inputs in Nintendo Switch games by plugging in a [Teensy](https://www.pjrc.com/teensy) into a Switch.
+
+## Setup
+* Clone the [NSGadget_Teensy](https://github.com/gdsports/NSGadget_Teensy) repo and create the "NS Gamepad" USB Type in Teensyduino
+  * For Windows or Linux, follow the instructions in that repo.
+  * For MacOS, just install Teensyduino, then copy the contents of `hardware` from NSGadget_Teensy to the correct files in Teensyduino.app's `hardware` directory.
+* Go to Tools > USB Type, then select "NS Gamepad."
+* Open a script, go to Tools > Verify/Compile, then Program and Reboot in the Teensyduino UI.
---
+++ botw/view_memory/view_memory.ino
@@ -0,0 +1,84 @@
+#include "usb_nsgamepad.h"
+
+#define PRESS_DELAY 500
+#define FRAME_MS 33 // 30 fps
+
+void pressButton(NSButtons b, int d)
+{
+  NSGamepad.press(b);
+  NSGamepad.write();
+  delay(d);
+  NSGamepad.release(b);
+  NSGamepad.write();
+}
+
+void pressButtonPrecise(NSButtons b, int numFrames)
+{
+  noInterrupts();
+  uint32_t wait_begin_at = systick_millis_count;
+  interrupts();
+  pressButton(b, FRAME_MS);
+  noInterrupts();
+  uint32_t delay_time = int((FRAME_MS * numFrames) - (systick_millis_count - wait_begin_at));
+  interrupts();
+  delay(delay_time); 
+}
+
+void pressTriggers(int d)
+{
+  NSGamepad.press(NSButton_LeftTrigger);
+  NSGamepad.press(NSButton_RightTrigger);
+  NSGamepad.write();
+  delay(d);
+  NSGamepad.release(NSButton_LeftTrigger);
+  NSGamepad.release(NSButton_RightTrigger);
+  NSGamepad.write();
+}
+
+void confirmController()
+{
+  // Send it some input, then wait, so the Switch begins the change controller menu
+  pressButton(NSButton_Y, PRESS_DELAY);
+  delay(PRESS_DELAY);
+  pressButton(NSButton_Y, PRESS_DELAY);
+  delay(PRESS_DELAY);
+  pressButton(NSButton_Y, PRESS_DELAY);
+  delay(PRESS_DELAY);
+  pressTriggers(300);
+  delay(PRESS_DELAY);
+  pressButton(NSButton_A, PRESS_DELAY);
+}
+
+void botwViewMemoryPrompt()
+{
+  pressButtonPrecise(NSButton_A, 6);
+  pressButtonPrecise(NSButton_A, 10);
+  pressButtonPrecise(NSButton_A, 9);
+
+  NSGamepad.dPad(NSGAMEPAD_DPAD_RIGHT);
+  NSGamepad.write();
+
+  delay(FRAME_MS);
+
+  NSGamepad.press(NSButton_A);
+  NSGamepad.write();
+
+  delay(FRAME_MS);
+
+  NSGamepad.release(NSButton_A);
+  NSGamepad.write();
+  NSGamepad.dPad(NSGAMEPAD_DPAD_CENTERED);
+  NSGamepad.write();
+}
+
+void setup() {
+  // Sends a clean HID report to the host.
+  NSGamepad.begin();
+  confirmController();
+  delay(3000);
+  botwViewMemoryPrompt();
+}
+
+void loop() {
+  NSGamepad.loop();
+}
